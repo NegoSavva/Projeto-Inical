2ed215b0877a2e5c4b0c55737497d87f6a9ffa3f
NegoSavva <joao.vitor.nicolay@gmail.com>
2024-03-19 08:39:57 -0300

    mudaca

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false );
 var nome = document.getElementById("nome");
 
+var saida = document.getElementById("saida-de-dados");
+
+var email = document.getElementById("email");
+
+var telefone = document.getElementById("telefone");
+
+var cep = document.getElementById("cep");
+
+var logadouro = document.getElementById("logadouro");
+
+var numero = document.getElementById("numero");
+
+var complemento = document.getElementById("complemento");
+
+var bairro = document.getElementById("bairro");
+
+var cidade = document.getElementById("cidade");
+
+
 function alertar(event){
-    alert("voce clicou no botão !!!" + nome.value);
+   // alert("voce clicou no botão !!!" + nome.value);
+   saida.innerText = "Nome: " + nome.value + "\n Email" + email.value
+   + "\n telefone" + telefone.value + "\n cep" + cep.value 
+   + "\n logadouro" + logadouro.value
+   + "\n numero" + numero.value
+   + "\n complemento" + complemento.value
+   + "\n bairro" + bairro.value
+   + "\n cidade" + cidade.value
+   ; 
 }
